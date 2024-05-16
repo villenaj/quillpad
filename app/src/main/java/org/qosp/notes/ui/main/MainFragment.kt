79f4c9db1430e16000195005477bd786ae63ed42
Joel Villena <villenajv43@gmail.com>
2024-05-16 16:02:05 +0800

    test cleanup

--- app/src/main/java/org/qosp/notes/ui/main/MainFragment.kt
+++ app/src/main/java/org/qosp/notes/ui/main/MainFragment.kt
@@ -1,10 +1,6 @@
 package org.qosp.notes.ui.main
 
 import android.content.Intent
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.RectF
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
@@ -13,7 +9,6 @@ import android.view.MenuItem
 import android.view.View
 import androidx.annotation.ColorInt
 import androidx.appcompat.widget.Toolbar
-import androidx.core.graphics.drawable.toBitmap
 import androidx.core.view.ViewCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.clearFragmentResult
@@ -23,13 +18,6 @@ import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavDirections
 import androidx.navigation.fragment.FragmentNavigatorExtras
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.ItemTouchHelper
-import androidx.recyclerview.widget.ItemTouchHelper.ACTION_STATE_DRAG
-import androidx.recyclerview.widget.ItemTouchHelper.ACTION_STATE_SWIPE
-import androidx.recyclerview.widget.ItemTouchHelper.DOWN
-import androidx.recyclerview.widget.ItemTouchHelper.LEFT
-import androidx.recyclerview.widget.ItemTouchHelper.RIGHT
-import androidx.recyclerview.widget.ItemTouchHelper.UP
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.google.android.material.appbar.AppBarLayout
@@ -47,14 +35,9 @@ import org.qosp.notes.ui.common.AbstractNotesFragment
 import org.qosp.notes.ui.recorder.RECORDED_ATTACHMENT
 import org.qosp.notes.ui.recorder.RECORD_CODE
 import org.qosp.notes.ui.recorder.RecordAudioDialog
-import org.qosp.notes.ui.tasks.TaskViewHolder
-import org.qosp.notes.ui.tasks.TasksAdapter
 import org.qosp.notes.ui.utils.ChooseFilesContract
 import org.qosp.notes.ui.utils.TakePictureContract
-import org.qosp.notes.ui.utils.dp
-import org.qosp.notes.ui.utils.getDrawableCompat
 import org.qosp.notes.ui.utils.navigateSafely
-import org.qosp.notes.ui.utils.resolveAttribute
 import org.qosp.notes.ui.utils.viewBinding
 import javax.inject.Inject
 
@@ -62,119 +45,10 @@ import javax.inject.Inject
 open class MainFragment : AbstractNotesFragment(R.layout.fragment_main) {
     private val binding by viewBinding(FragmentMainBinding::bind)
     @ColorInt
-    private var backgroundColor: Int = Color.TRANSPARENT
     override val currentDestinationId: Int = R.id.fragment_main
     override val model: MainViewModel by viewModels()
-    private lateinit var tasksAdapter: TasksAdapter
     open val notebookId: Long? = null
 
-    private val itemTouchHelper = ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(UP or DOWN, LEFT or RIGHT) {
-        override fun isLongPressDragEnabled() = false
-
-        override fun isItemViewSwipeEnabled() = true
-
-        override fun getSwipeThreshold(viewHolder: RecyclerView.ViewHolder) = 0.5F
-
-        override fun getSwipeEscapeVelocity(defaultValue: Float) = 3 * defaultValue
-
-        override fun getSwipeVelocityThreshold(defaultValue: Float) = defaultValue / 3
-
-        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-            tasksAdapter.tasks.removeAt(viewHolder.bindingAdapterPosition)
-//            model.updateTaskList(tasksAdapter.tasks)
-            tasksAdapter.notifyItemRemoved(viewHolder.bindingAdapterPosition)
-            tasksAdapter.notifyItemRangeChanged(viewHolder.bindingAdapterPosition, tasksAdapter.tasks.size - 1)
-        }
-
-        override fun onMove(
-            recyclerView: RecyclerView,
-            viewHolder: RecyclerView.ViewHolder,
-            target: RecyclerView.ViewHolder,
-        ): Boolean {
-            tasksAdapter.moveItem(viewHolder.bindingAdapterPosition, target.bindingAdapterPosition)
-            return true
-        }
-
-        override fun onChildDraw(
-            c: Canvas,
-            recyclerView: RecyclerView,
-            viewHolder: RecyclerView.ViewHolder,
-            dX: Float,
-            dY: Float,
-            actionState: Int,
-            isCurrentlyActive: Boolean,
-        ) {
-            when (actionState) {
-                ACTION_STATE_DRAG -> {
-                    val top = viewHolder.itemView.top + dY
-                    val bottom = top + viewHolder.itemView.height
-                    if (top > 0 && bottom < recyclerView.height) {
-                        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
-                    }
-                }
-
-                ACTION_STATE_SWIPE -> {
-                    val newDx = dX / 3
-                    val p = Paint().apply { color = context?.resolveAttribute(R.attr.colorTaskSwipe) ?: Color.RED }
-                    val itemView = viewHolder.itemView
-                    val icon = context?.getDrawableCompat(R.drawable.ic_indicator_delete_task)?.toBitmap()
-                    val height = itemView.bottom - itemView.top
-                    val size = (24).dp(requireContext())
-
-                    if (dX < 0) {
-                        val background = RectF(
-                            itemView.right.toFloat() + newDx,
-                            itemView.top.toFloat(),
-                            itemView.right.toFloat(),
-                            itemView.bottom.toFloat()
-                        )
-                        c.drawRect(background, p)
-
-                        val iconRect = RectF(
-                            background.right - size - 16.dp(requireContext()),
-                            background.top + (height - size) / 2,
-                            background.right - 16.dp(requireContext()),
-                            background.bottom - (height - size) / 2,
-                        )
-                        if (icon != null) c.drawBitmap(icon, null, iconRect, p)
-                    } else if (dX > 0) {
-                        val background = RectF(
-                            itemView.left.toFloat(),
-                            itemView.top.toFloat(),
-                            newDx,
-                            itemView.bottom.toFloat()
-                        )
-                        c.drawRect(background, p)
-                        val iconRect = RectF(
-                            background.left + 16.dp(requireContext()),
-                            background.top + (height - size) / 2,
-                            background.left + size + 16.dp(requireContext()),
-                            background.bottom - (height - size) / 2,
-                        )
-                        if (icon != null) c.drawBitmap(icon, null, iconRect, p)
-                    }
-                    return super.onChildDraw(c, recyclerView, viewHolder, newDx, dY, actionState, isCurrentlyActive)
-                }
-            }
-        }
-
-        override fun onSelectedChanged(viewHolder: RecyclerView.ViewHolder?, actionState: Int) {
-            super.onSelectedChanged(viewHolder, actionState)
-
-            (viewHolder as TaskViewHolder?)?.let { vh ->
-                vh.taskBackgroundColor = backgroundColor
-                vh.isBeingMoved = true
-            }
-        }
-
-        override fun clearView(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder) {
-            super.clearView(recyclerView, viewHolder)
-            (viewHolder as TaskViewHolder?)?.let {
-                if (it.isBeingMoved) it.isBeingMoved = false
-            }
-//            model.updateTaskList(tasksAdapter.tasks)
-        }
-    })
 
     private val chooseFileLauncher = registerForActivityResult(ChooseFilesContract) { uris ->
         if (uris.isEmpty()) return@registerForActivityResult
